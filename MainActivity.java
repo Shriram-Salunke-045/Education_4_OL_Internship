72502e13e30867b298d7b527b5001d30c4a7a792
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2023-05-25 12:48:05 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,40 +1,23 @@
-package com.example.edu_task1;
+package com.example.educationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btnLogOut;
-    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogOut = findViewById(R.id.btnLogout);
-        mAuth = FirebaseAuth.getInstance();
-
-        btnLogOut.setOnClickListener(view ->{
-            mAuth.signOut();
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        });
-
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser user = mAuth.getCurrentUser();
-        if (user == null){
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        }
+    public void next(View view) {
+        Intent i =new Intent(MainActivity.this,Login.class);
+        startActivity(i);
+        finish();
     }
 }
